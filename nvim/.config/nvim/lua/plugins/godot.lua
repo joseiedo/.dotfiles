de9f48c38f67127e7272e751fa15167b129e2585
iedo <iedoduarte1@gmail.com>
2026-03-31 22:49:58 -0300

    godot

--- nvim/.config/nvim/lua/plugins/godot.lua
+++ nvim/.config/nvim/lua/plugins/godot.lua
@@ -5,32 +5,32 @@ M.source = 'https://github.com/lommix/godot.nvim'
 function M.setup()
   -- -- Skip godot.nvim's lspconfig-based LSP setup (deprecated in nvim 0.11+)
   -- -- and configure gdscript LSP ourselves via vim.lsp.config
-  -- require('godot.lsp').setup = function() end
-  --
-  -- require('godot').setup {
-  --   bin = 'godot',
-  --   dap = {
-  --     host = '127.0.0.1',
-  --     port = 6006,
-  --   },
-  --   gui = {
-  --     console_config = {
-  --       anchor = 'SW',
-  --       border = 'double',
-  --       col = 1,
-  --       height = 10,
-  --       relative = 'editor',
-  --       row = 99999,
-  --       style = 'minimal',
-  --       width = 99999,
-  --     },
-  --   },
-  --   expose_commands = true,
-  -- }
-  --
-  -- -- Set up gdscript LSP the modern way
-  -- vim.lsp.config('gdscript', {})
-  -- vim.lsp.enable 'gdscript'
+  require('godot.lsp').setup = function() end
+
+  require('godot').setup {
+    bin = 'godot',
+    dap = {
+      host = '127.0.0.1',
+      port = 6006,
+    },
+    gui = {
+      console_config = {
+        anchor = 'SW',
+        border = 'double',
+        col = 1,
+        height = 10,
+        relative = 'editor',
+        row = 99999,
+        style = 'minimal',
+        width = 99999,
+      },
+    },
+    expose_commands = true,
+  }
+
+  -- Set up gdscript LSP the modern way
+  vim.lsp.config('gdscript', {})
+  vim.lsp.enable 'gdscript'
 end
 
 return M
