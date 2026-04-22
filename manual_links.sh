b84a78b5c9e664d8d2258bc5d1e59c47e46545ff
iedo <iedoduarte1@gmail.com>
2026-04-22 18:44:01 -0300

    wip

---
+++ manual_links.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Creating manual links for the project..."
+
+echo "1. Mouseless"
+ln -s  ~/.dotfiles/mouseless/configs ~/Library/Containers/net.sonuscape.mouseless/Data/.mouseless/configs
