55e1ba50c69b3dd8c8620750d43ec68099d442f0
iedo <iedoduarte1@gmail.com>
2026-07-23 23:28:40 -0300

    add gruvbox theme

--- nvim/.config/nvim/plugin/colorscheme.lua
+++ nvim/.config/nvim/plugin/colorscheme.lua
@@ -1,7 +1,16 @@
 vim.o.termguicolors = true
 
-vim.cmd.colorscheme 'default'
+Config.now(function()
+  vim.pack.add { 'https://github.com/sainnhe/gruvbox-material' }
+  -- require('gruvbox-material').setup {}
+
+  vim.g.gruvbox_material_enable_italic = true
+  vim.g.gruvbox_material_background = 'hard'
+  vim.cmd.colorscheme 'gruvbox-material'
+end)
+--
+-- vim.cmd.colorscheme 'default'
 
 -- Comments should be more visible. Copilot suggestions should be more subtle.
-vim.api.nvim_set_hl(0, 'Comment', { fg = '#c4a96b' })
+vim.api.nvim_set_hl(0, 'Comment', { fg = '#f05330' })
 vim.api.nvim_set_hl(0, 'CopilotSuggestion', { fg = '#6e6e6e', italic = true })
--- nvim/.config/nvim/plugin/oil.lua
+++ nvim/.config/nvim/plugin/oil.lua
@@ -1,14 +1,14 @@
 if false then
-Config.later(function()
-  vim.pack.add { 'https://github.com/stevearc/oil.nvim' }
+  Config.later(function()
+    vim.pack.add { 'https://github.com/stevearc/oil.nvim' }
 
-  require('oil').setup {
-    default_file_explorer = true,
-    view_options = {
-      show_hidden = true,
-    },
-  }
+    require('oil').setup {
+      default_file_explorer = true,
+      view_options = {
+        show_hidden = true,
+      },
+    }
 
-  vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-end)
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+  end)
 end
